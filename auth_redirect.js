e64a904cd53fce3841ab4fc3e6e8625dd21ef9c1
monsieurbot <71845656+monsieurbot@users.noreply.github.com>
2024-09-23 14:32:55 +0200

    Delete auth_redirect.js

--- auth_redirect.js
+++
@@ -1,9 +0,0 @@
-angular.module('application')
-  .run(['$rootScope', '$state', 'AuthService', function ($rootScope, $state, AuthService) {
-    $rootScope.$on('$stateChangeStart', function (event, toState) {
-      if (toState.name === 'app.public.projects_list' && !AuthService.isAuthenticated()) {
-        event.preventDefault(); // Empêche la navigation vers la page des projets
-        $rootScope.login(event); // Ouvre la popup de connexion
-      }
-    });
-  }]);
