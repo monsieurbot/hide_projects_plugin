6dfeb0c0c1ced37b55494b159341be7a7c64aa02
monsieurbot <71845656+monsieurbot@users.noreply.github.com>
2024-09-23 14:33:42 +0200

    Update hide_projects_plugin.rb

--- lib/hide_projects_plugin.rb
+++ lib/hide_projects_plugin.rb
@@ -1,1 +1,16 @@
+module HideProjectsPlugin
+  class Engine < ::Rails::Engine
+    isolate_namespace HideProjectsPlugin
+
+    # Enregistrement des assets du plugin
+    initializer 'hide_projects_plugin.assets.precompile' do |app|
+      app.config.assets.precompile += %w( hide_projects_plugin/auth_redirect.js )
+    end
+
+    # Ajout du chemin des vues du plugin
+    initializer 'hide_projects_plugin.append_view_paths' do |app|
+      app.config.paths['app/views'].unshift File.expand_path('../../app/views', __dir__)
+    end
+  end
+end
 
